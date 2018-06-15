0e779dbdd3eab0b3cc92d1f76620052229d56bed
Garnett Puett <gpuett@gmail.com>
2018-06-15 13:44:07 -0700

    addingReviewSetsId test

---
+++ src/test/java/dao/Sql2oReviewDaoTest.java
@@ -0,0 +1,88 @@
+package dao;
+
+import org.junit.*;
+import org.sql2o.*;
+import models.*;
+
+import static org.junit.Assert.*;
+
+public class Sql2oReviewDaoTest {
+    private static Connection conn;
+    private static Sql2oReviewDao reviewDao;
+    private static Sql2OPartDao partDao;
+
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        String connectionString = "jdbc:postgresql://localhost:5432/pc_parts_test";
+        Sql2o sql2o = new Sql2o(connectionString, null, null);
+        reviewDao = new Sql2oReviewDao(sql2o);
+        partDao = new Sql2OPartDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("clearing database");
+        reviewDao.clearAll();
+        partDao.clearAll();
+    }
+
+    @AfterClass
+    public static void shutDown() throws Exception {
+        conn.close();
+        System.out.println("connection closed");
+    }
+
+    @Test
+    public void addingReviewSetsId() {
+        Review review = setupReview();
+        int originalId = review.getId();
+        int foundId = reviewDao.findById(review.getId()).getId();
+        assertEquals(foundId, originalId);
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void getAllReviewsByPartId() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+
+    @Test
+    public void clearAll() {
+    }
+
+    @Test
+    public void getAllReviewsByPartSortedNewestToOldest() {
+    }
+
+    //helpers
+
+    public Review setupReview() {
+        Review review = new Review("Works great!", "John", 4, 20);
+        reviewDao.add(review);
+        return review;
+    }
+
+    public Part setupPart() {
+        Part part = new Part("AMD", "RYZEN 7", 219);
+        partDao.add(part);
+        return part;
+    }
+
+    public Review setupReviewForPart(Part part) {
+        Review review = new Review("Works great!", "John", 4, part.getId());
+        reviewDao.add(review);
+        return review;
+    }
+}
