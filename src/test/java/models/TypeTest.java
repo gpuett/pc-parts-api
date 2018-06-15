749f820782a3f462ca9e43ca9f43da29476afdb3
Garnett Puett <gpuett@gmail.com>
2018-06-15 14:20:24 -0700

    type setId test

--- src/test/java/models/TypeTest.java
+++ src/test/java/models/TypeTest.java
@@ -21,6 +21,9 @@ public class TypeTest {
 
     @Test
     public void setId() {
+        Type type = setupType();
+        type.setId(4);
+        assertEquals(4, type.getId());
     }
 
     //helpers
